9a214ad18072c16f3855f2a6b401cdedfa5a6bc8
Alexandre <alex.al.mar@hotmail.com>
2018-04-13 11:17:47 +0100

    Add files via upload

--- Prog_2.cpp
+++ Prog_2.cpp
@@ -26,6 +26,7 @@ bool match(string word, map<char, vector < string >> &map)
 	}
 	return false;
 }
+
 vector < string > wordsSized(map<char, vector < string >> &map, int wordSize) {
 	vector < string > final;
 	for (int k = 0; k < map.size(); k++) {
@@ -45,17 +46,6 @@ vector < string > wordsSized(map<char, vector < string >> &map, int wordSize) {
 	return final;
 }
 
-vector < string > checkLetters(vector < string > list, string word) {
-	for (int i = 0; i < list.size(); i++) {
-		string cWord = list[i];
-		string tempWord = word;
-		while (tempWord.size() > 0) {
-
-		}
-
-	}
-}
-
 string scramble(string word) {
 	string final;
 	srand(time(NULL));
@@ -67,8 +57,72 @@ string scramble(string word) {
 	return final;
 }
 
-//ANEXO C
-bool wildcardMatch(const char *str, const char *strWild)
+vector < string > scramble_find(vector < string > &list, string letters)
+{
+	vector < int > cont_letter;
+	string letters1 = letters, l_final;
+	vector < string > words;
+	vector < int > cont_final;
+	int cont = 0;
+	size_t i=0;
+
+	while( letters1.size() != 0)
+	{
+		char letter = letters1[i];
+		while (i != string::npos)
+		{
+			cont++;
+			letters1.erase(letters1.begin() + i);
+			i = letters1.find_first_of(letter);
+
+		}
+		l_final = l_final + letter;
+		cont_letter.push_back(cont);
+		cont = 0;
+		i = 0;
+	} 
+
+	for (i = 0; i < list.size(); i++)
+	{
+		size_t pos = list[i].find_first_not_of(letters);
+		if (pos == string::npos)
+			words.push_back(list[i]);
+	}	
+	
+	int j = 0, k=0;
+	while (k < words.size())
+	{
+		string w = words[k];
+		j = 0;
+		while (w.size() != 0)
+		{
+			char letter = l_final[j];
+			i = w.find_first_of(letter);
+
+			while (i != string::npos)
+			{
+				cont++;
+				w.erase(w.begin() + i);
+				i = w.find_first_of(letter);
+
+			}
+			cont_final.push_back(cont);
+			cont = 0;
+			j++;
+		
+		}
+		if (cont_final != cont_letter)
+			words.erase(words.begin() + k);
+		else
+			k++;
+
+		cont_final.clear();
+	}
+
+	return words;
+}
+
+/*ANEXO C*/bool wildcardMatch(const char *str, const char *strWild)
 {
 	// We have a special case where string is empty ("") and the mask is "*".
 	// We need to handle this too. So we can't test on !*str here.
@@ -117,6 +171,30 @@ bool wildcardMatch(const char *str, const char *strWild)
 	return !*str && !*strWild;
 }
 
+int frequency(map<char, vector < string >> &m, char letter)
+{
+	int k, i, cont = 0;
+	vector < string > list;
+	string word;
+	size_t pos;
+
+	for (k = 0; k < 26; k++)
+	{
+		list = m[abc[k]];
+		for (i = 0; i < list.size(); i++)
+		{
+			word = list[i];
+			pos = word.find_first_of(letter);
+			while (pos != string::npos)
+			{
+				cont++;
+				pos = word.find_first_of(letter, pos +1);
+			}
+		}
+	}
+	// menor frequencia: J - 1251, TEM 2 J' in Larger set//
+	return (int)((2 * cont) / 1251);
+}
 
 int main()
 {
@@ -140,7 +218,7 @@ int main()
 	f.close();
 
 	cout << "Choose one of the following actions: " << endl;
-	cout << "	(1) If the word belongs to the word list" << endl; 
+	cout << "	(1) If the word belongs to the word list" << endl;
 	cout << "	(2) Guess the word" << endl;
 	cout << "	(3) All the words that can be built using the set of the given letters" << endl;
 	cout << "	(4) Build a valid word" << endl;
@@ -156,9 +234,9 @@ int main()
 		cout << "Word: ";
 		cin >> word;
 		if (match(word, m) == true)
-			cout << "Existe" << endl;
+			cout << "The word " << word << " exist." <<endl;
 		else
-			cout << " Nao existe" << endl;
+			cout << "The word " << word << " don't exist." << endl;
 	}
 	else if (op == 2)
 	{
@@ -196,17 +274,104 @@ int main()
 		cout << "Letters to use: ";
 		cin >> letters;
 		vector < string > words;
+
 		words = wordsSized(m, letters.size());
-		cout << "Number of words with the same size: " << words.size() << endl;
+		//cout << "Number of words with the same size: " << words.size() << endl;
+		words = scramble_find(words, letters);
+		cout << "Number of words that can be built: " << words.size() << endl;
+		cout << "Words: ";
+
+		for (int i = 0; i < words.size(); i++)
+			cout << words[i] << " ";
+		cout << endl;
+	
+
 	}
 	else if (op == 4)
 	{
+		string larger_set;
+		string s;
+		string word, set;
+		int freq = 0;
+		int k, i, N, num;
 
+		cout << "Creating larger set ..." << endl;
+		for ( k = 0; k < 26; k++)
+		{
+			s = abc[k];
+			freq = frequency(m, abc[k]);
+			for (i = 0; i < freq; i++)
+			{
+				larger_set = larger_set +s;
+			}
+		}
 
+		cout << endl << "Size of the set, N: ";
+		cin >> N;
+
+		srand(time(NULL));
+		while (N != 0) 
+		{
+			num = rand() % (larger_set.size());
+			//cout << num<< endl;
+			N--;
+			set = set + larger_set[num];
+		}
+		
+		cout << endl << "  Set: " << set << endl;
+		cout << "  Try to build a word: ";
+		cin >> word;
+
+		if (match(word, m) == true)
+			cout << endl << "Correct! The word " << word << " exist." << endl;
+		else
+			cout << "Sorry... the word " << word << " don't exist." << endl;
+
+		
 	}
 	else if (op == 5)
 	{
+		string s1, s2;
+		vector < string > list;
+		vector < string > words;
+		size_t i, k;
 
+		cout << "String: ";
+		cin >> s1;
+		
+		for (i = 0; i < 26; i++)
+		{
+			if(abc[i] == s1[0])
+				list = m[abc[i]];
+		}
+
+		if (list.size() != 0)// string s1 começa por letra//
+		{
+			for (i = 0; i < list.size(); i++)
+			{
+				s2 = list[i];
+				if (wildcardMatch(s2.c_str(), s1.c_str()) == true)
+					words.push_back(s2);
+			}
+		}
+		else// string s1 começa por * ou ?//
+		{
+			for (k = 0; k < 26; k++)
+			{
+				list = m[abc[k]];
+				for (i = 0; i < list.size(); i++)
+				{
+					s2 = list[i];
+					if (wildcardMatch(s2.c_str(), s1.c_str()) == true)
+						words.push_back(s2);
+				}
+
+			}
+		}
+		cout << "Match with: "<< endl;
+		for (i = 0; i < words.size(); i++)
+			cout << "	" << words[i] << endl;
+		
 	}
 	else
 		cout << endl << "ERRO" << endl;
